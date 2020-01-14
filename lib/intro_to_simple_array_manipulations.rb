a6fcdefe91bfef524748d2360ccb42d2922ac7bf
Pedro David Garcia Lopez <reddevil_cero@hotmail.com>
2020-01-14 13:15:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -50,19 +50,19 @@ end
 def using_flatten(array)
 
   array.flatten
-  
+
 end
 
 def using_delete(array, string)
 
   array.delete(string)
-  
+
 end
 
 def using_delete_at(array, index)
 
   array.delete_at(index)
-  
+
 end
 
 
@@ -79,19 +79,3 @@ end
 
 
 1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
